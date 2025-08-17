e32c7ee19c3a1f9d9c950bd1539814eafe392e90
fruzozzi3 <124436222+fruzozzi3@users.noreply.github.com>
2025-08-17 13:39:37 +0300

    Update MainActivity.kt

--- app/src/main/java/com/example/mypiggybank/MainActivity.kt
+++ app/src/main/java/com/example/mypiggybank/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.example.mypiggybank
 
+import androidx.compose.ui.text.input.KeyboardOptions
+import androidx.compose.ui.text.input.KeyboardType
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
